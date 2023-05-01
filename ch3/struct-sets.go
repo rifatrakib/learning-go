3ab98c798808e2e30e14fb2523218f1087fa0791
rifatrakib <Rakib.1508@outlook.com>
2023-05-01 12:32:21 +0600

    learn to implement set data structure with map

---
+++ ch3/struct-sets.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	intSet := map[int]struct{}{}
+	vals := []int{5, 10, 2, 5, 8, 7, 3, 9, 1, 2, 10}
+	for _, v := range vals {
+		intSet[v] = struct{}{}
+	}
+
+	if _, ok := intSet[5]; ok {
+		fmt.Println("5 is in the set")
+	}
+}
