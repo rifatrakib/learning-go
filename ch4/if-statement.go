b4a3a2175b59479f830068edb02f3f1f58fe8f5b
rifatrakib <Rakib.1508@outlook.com>
2023-05-02 19:00:23 +0600

    learn if statement and scoping a variable to the entire block

---
+++ ch4/if-statement.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	n := rand.Intn(10)
+	if n == 0 {
+		fmt.Println("That's too low")
+	} else if n > 5 {
+		fmt.Println("That's too big:", n)
+	} else {
+		fmt.Println("That's a good number:", n)
+	}
+
+	if m := rand.Intn(10); m == 0 {
+		fmt.Println("That's too low")
+	} else if m > 5 {
+		fmt.Println("That's too big:", m)
+	} else {
+		fmt.Println("That's a good number:", m)
+	}
+}
