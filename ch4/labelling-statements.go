e3fd15d52a9036b96b1be749d9ffd4b1aed28478
rifatrakib <Rakib.1508@outlook.com>
2023-05-04 13:09:28 +0600

    labelled statements as an alternative to break

---
+++ ch4/labelling-statements.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	samples := []string{"hello", "apple_n!"}
+
+outer:
+	for _, sample := range samples {
+		for i, r := range sample {
+			fmt.Println(i, r, string(r))
+			if r == 'l' {
+				continue outer
+			}
+		}
+		fmt.Println()
+	}
+}
